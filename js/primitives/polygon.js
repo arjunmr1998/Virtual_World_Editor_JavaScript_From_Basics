894d73ce70e992c9fd3a13a9230ace40c4ce6345
Arjun More <arjunmr98@gmail.com>
2026-08-28 05:57:35 +0530

    enevelope_for_buildings

--- js/primitives/polygon.js
+++ js/primitives/polygon.js
@@ -129,6 +129,18 @@ class Polygon {
 
     }
 
+    intersectsPoly(poly){
+        for(let s1 in this.segments){
+            for(let s2 in this.poly){
+                if(getIntersection(s1.p1,s1.p2,s2.p1,s2.p2)){
+                    return true;
+                }
+            }
+        }
+        return false;
+
+    }
+
 
 
 
--- js/world.js
+++ js/world.js
@@ -98,7 +98,33 @@ class World{
 
         }
 
-        return supports
+        const bases=[];
+        for(const seg of supports){
+            bases.push(new Envelope(seg,this.buildingWidth).poly);
+
+
+        }
+         
+        for(let i=0;i<bases.length-1;i++){
+            for(let j=i+1;i<bases.length;j++){
+                if(bases[i].intersectsPoly(bases[j])){
+                    bases.splice(j);
+                    j--;
+                }
+            }
+
+
+
+        }
+
+
+
+
+
+
+
+
+        return bases
 
     }
 
