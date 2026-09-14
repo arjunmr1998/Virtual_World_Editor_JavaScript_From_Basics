ae0b85086da81c45c842d2be5893a0edda764be0
Arjun More <arjunmr98@gmail.com>
2026-09-14 15:46:18 +0530

    getFake3D_points

--- js/math/utils.js
+++ js/math/utils.js
@@ -120,3 +120,10 @@ function lerp2D(A,B,t){
     return new Point(lerp(A.x, B.x ,t) , lerp(A.y, B.y,t));
 
 }
+
+function getFake3dPoint(point, viewPoint, height) {
+   const dir = normalize(subtract(point, viewPoint));
+   const dist = distance(point, viewPoint);
+   const scaler = Math.atan(dist / 300) / (Math.PI / 2);
+   return add(point, scale(dir, height * scaler));
+}
